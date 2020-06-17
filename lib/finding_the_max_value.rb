cddef525753b16e00b22dc1dce8287e1345432f5
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-06-17 14:17:33 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,9 +1,8 @@
 def find_max_value(array)
   count = 0 
   sorted_array = array.sort 
-  while count < array.length do 
-    
-    count += 1 
-  end
+  #while count < array.length do 
+    #count += 1 
+  #end
   sorted_array[-1]
 end
